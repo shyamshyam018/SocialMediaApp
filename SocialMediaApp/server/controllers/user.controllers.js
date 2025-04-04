50bc1e45186049116893bc1f85c1834f936f0237
shyamshyam018 <shyamrajsr2004@gmail.com>
2025-04-04 21:09:23 +0530

    completing the auth module

--- SocialMediaApp/server/controllers/user.controllers.js
+++ SocialMediaApp/server/controllers/user.controllers.js
@@ -1,41 +1,52 @@
-import { updateUser , deleteUser } from "../services/user.service.js";
+import { updateUser, deleteUser, getUser } from "../services/user.service.js";
 
-export const updateUserController = async (req , res) => {
+export const updateUserController = async (req, res) => {
     try {
-        const user  = await updateUser(req.params.id , req.body);
-        res.status(200).json({
-            user,
-            message: "Account has been updated Successfully!"
-        })
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            const user = await updateUser(req.params.id, req.body);
+            return res.status(200).json({
+                user,
+                message: "Account has been updated Successfully!"
+            });
+        } 
+        return res.status(403).json({ message: "You can only update YOUR account!" });
     } catch (error) {
         console.log(error);
-        res.status(500).json(error);
+        res.status(500).json({ error: error.message });
     }
-}
-
+};
 
 export const deleteUserController = async (req, res) => {
     try {
-        const user = await deleteUser(req.params.id);
-        res.status(200).json({
-            user ,
-            message: "Account has been deleted Successfully!"
-        })
+    
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            const user = await deleteUser(req.params.id);
+            return res.status(200).json({
+                user,
+                message: "Account has been deleted Successfully!"
+            });
+        } 
+        return res.status(403).json({ message: "You can only delete YOUR account!" });
     } catch (error) {
         console.log(error);
-        res.status(500).json(error);
+        res.status(500).json({ error: error.message });
     }
-}
+};
 
 export const getUserController = async (req, res) => {
     try {
-        const user = await getUser(req.params.id);
-        res.status(200).json({
-            user ,
-            message: "Account has been found Successfully!"
-        })
+        
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            const user = await getUser(req.params.id);
+            const { password, ...data } = user.toObject();
+            return res.status(200).json({
+                data,
+                message: "Account has been found Successfully!"
+            });
+        } 
+        return res.status(403).json({ message: "You can only view YOUR account!" });
     } catch (error) {
         console.log(error);
-        res.status(500).json(error);
+        res.status(500).json({ error: error.message });
     }
-}
+};
--- SocialMediaApp/server/services/auth.service.js
+++ SocialMediaApp/server/services/auth.service.js
@@ -1,25 +1,27 @@
-
-
 import UserModel from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
 // Register User
 export const registerUser = async (body) => {
-        const hashedPassword = bcrypt.hashSync(body.password , 10);
-        const newUser = new UserModel({
-            username : body.username , 
-            email : body.email ,
-            password : hashedPassword , 
-        });
-        await newUser.save();
-        return newUser;
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    const newUser = new UserModel({
+        username: body.username,
+        email: body.email,
+        password: hashedPassword,
+    });
+    await newUser.save();
+    return newUser;
 };
 
-
+// Login User
 export const loginUser = async (body) => {
-    const user = await UserModel.findOne({email:body.email});
-    !user && res.status(404).json("User not Found!");
-    const passwordCheck = await bcrypt.compare(body.password,user.password);
-    !passwordCheck && res.status(400).json("Wrong Password");
+    const user = await UserModel.findOne({ email: body.email });
+    if (!user) {
+        throw new Error("User not Found!");
+    }
+    const passwordCheck = await bcrypt.compare(body.password, user.password);
+    if (!passwordCheck) {
+        throw new Error("Wrong Password!"); 
+    }
     return user;
-}
+};
